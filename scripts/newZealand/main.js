28e238d331a4ac24a9302c74f91e9700e0d95135
garrettfreshwater-nss <gfresh.nss@gmail.com>
2019-12-03 13:31:29 -0600

    finished leaders components and got automation working.

--- scripts/newZealand/main.js
+++ scripts/newZealand/main.js
@@ -1,6 +1,8 @@
 import CityListComponent from "./cities/CityList.js";
 import LandmarkListComponent from "./landmarks/LandmarksList.js";
+import LeadersListComponent from "./leaders/LeadersList.js";
 
 
 CityListComponent()
 LandmarkListComponent()
+LeadersListComponent()
