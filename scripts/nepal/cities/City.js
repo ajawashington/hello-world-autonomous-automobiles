ed38f9d1903e9ef9bcf09d0a83b16393813d1af6
macgibbons <macgibbons@gmail.com>
2019-12-03 10:45:26 -0600

    added data providers for all js folders in Nepal

---
+++ scripts/nepal/cities/City.js
@@ -0,0 +1,9 @@
+const CityComponent = (city) => {
+    return `
+        <div class="cities">
+            <div class="fish__name">${city.city}</div>
+        </div>
+    `
+}
+
+export default CityComponent
