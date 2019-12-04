49c47b6a11e35804d145bad34e5bdb3d44ce6561
AdiGarmendia <dejima@gmail.com>
2019-12-04 10:57:17 -0600

    dialogue fixes on Nepal, NZ and Mad

--- scripts/newZealand/landmarks/Landmarks.js
+++ scripts/newZealand/landmarks/Landmarks.js
@@ -1,10 +1,10 @@
 const LandmarkComponent = (landmark) => {
     return `
         <div class="landmark__name">${landmark.landmark}</div>
-        <button id="button--${landmark.city.replace(/\s/g, '')}">Learn More!</button>
+        <button id="button--${landmark.landmark.replace(/\s/g, '')}">Learn More!</button>
 
         <dialog class="dialog--landmark" id="details--${landmark.landmark}">
-        <div>City: ${landmark.city}</div>
+        <div>Location: ${landmark.city}</div>
 
         <button class="close__dialog">Close Dialog</button>
         </dialog>
