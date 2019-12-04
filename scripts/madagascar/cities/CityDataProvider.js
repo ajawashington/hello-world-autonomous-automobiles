d3d897e95d6bed1e7eb962faf33be3063daaca26
AdiGarmendia <dejima@gmail.com>
2019-12-04 12:57:28 -0600

    all

--- scripts/madagascar/cities/CityDataProvider.js
+++ scripts/madagascar/cities/CityDataProvider.js
@@ -3,19 +3,19 @@ const cities = [{
     city: "Antananarivo",
     population: "1.3 Million",
     mayor: "Lalao Ravalomanana",
-    imgUrl: '/images/pok.jpg'
+    imgUrl: '/images/madCityAnt.jpg'
 },
 {
     city: "Toamasina",
     population: "390K",
     mayor: "Roland Ratsiraka",
-    imgUrl: '/images/pok.jpg'
+    imgUrl: '/images/madCityToa.jpg'
 },
 {
     city: "Antsirabe",
     population: "257K",
     mayor: "Olga Ramalason",
-    imgUrl: '/images/pok.jpg'
+    imgUrl: '/images/madCityAntsi.jpg'
 },
 
 // change these ^^ to your cities
